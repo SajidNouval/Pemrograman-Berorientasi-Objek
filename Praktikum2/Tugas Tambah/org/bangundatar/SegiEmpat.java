1c0d4969f8720f1265a5dd800227e6be7341bef9
SajidNouval <160684552+SajidNouval@users.noreply.github.com>
2024-03-02 20:13:04 +0700

    Delete Praktikum2/Tugas Tambah/org directory

--- Praktikum2/Tugas Tambah/org/bangundatar/SegiEmpat.java
+++
@@ -1,4 +0,0 @@
-package org.bangundatar;
-
-public class SegiEmpat {
-}
--- Praktikum2/Tugas Tambah/org/main/MLimasSegiEmpat.java
+++
@@ -1,9 +0,0 @@
-package org.main;
-import org.bangunruang.LimasSegiEmpat;
-
-public class MLimasSegiEmpat {
-    public static void main(String[] args) {
-        LimasSegiEmpat limas = new LimasSegiEmpat(6, 9);
-        System.out.println("Volume Limas Segi Empat: " + limas.hitungvolume());
-    }
-}

3cc2934b1ba1bb2136ccbcf98a863f77ad1a46e9
SajidNouval <160684552+SajidNouval@users.noreply.github.com>
2024-03-13 17:07:57 +0700

    Add files via upload

---
+++ Praktikum 4/Tugas Modul/src/bangundatar/PersegiPanjang.java
@@ -0,0 +1,23 @@
+//Sajid Nouval - 24060122140110
+
+package bangundatar;
+import poligon.Poligon;
+
+public class PersegiPanjang extends Poligon{
+    private double panjang, lebar;
+
+    public PersegiPanjang(double panjang, double lebar, int jumlahSisi){
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.jumlahSisi = jumlahSisi;
+    }
+
+    public double hitungLuas(){
+        return panjang * lebar;
+    }
+
+    public void printInfo(){
+        System.out.println("Bangun Persegi Panjang bersisi " + this.getJumlahSisi());
+    }
+
+}
